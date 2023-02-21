c35ea29ddb91c70cc42d7a100eb791f686113bc7
Yuan-Chu Tai <chobits@itri.org.tw>
2023-02-21 16:29:42 +0800

    test ok

--- WarRoomDemo/Program.cs
+++ WarRoomDemo/Program.cs
@@ -5,22 +5,22 @@ namespace WarRoomDemo
 {
     public class Vehicle
     {
-        public string? vehicleUid { get; set; }
+        public string vehicleUid { get; set; } = "";
         public double[]? position { get; set; }
-        public string? type { get; set; }
+        public string type { get; set; } = "copter";
         public string? swarmUid { get; set; }
         public bool isLeader { get; set; } = false;
     }
 
     public class Status
     {
-        public string? uid { get; set; }
+        public string uid { get; set; } = "";
         public DateTime timestamp { get; set; }
         public string type { get; set; } = "dgsStatus";
         public class StatusData
         {
-            public string? systemUid { get; set; }
-            public int ttl { get; set; }
+            public string systemUid { get; set; } = "";
+            public int ttl { get; set; } = 1;
             public List<Vehicle> vehicles { get; set; } = new List<Vehicle>();
         };
         public StatusData data { get; set; } = new StatusData();
@@ -30,19 +30,33 @@ namespace WarRoomDemo
     {
         static void Main(string[] args)
         {
-            /*ClientWebSocket ws = new ClientWebSocket();
+            ClientWebSocket ws = new ClientWebSocket();
             Console.WriteLine("connecting...");
-            Task task = ws.ConnectAsync(new Uri("ws://120.0.0.1:55688"), CancellationToken.None);
+            string uri = "ws://127.0.0.1:55688/Third";
+            if (args.Length > 0)
+            {
+                uri = args[0];
+            }
+            Task task = ws.ConnectAsync(new Uri(uri), CancellationToken.None);
             task.Wait();
-            Console.WriteLine("connected");*/
+            Console.WriteLine("connected");
 
             var status = new Status();
             status.data.vehicles.Add(new Vehicle
             {
-                vehicleUid = "haha",
-                position = new double[2] { 5.0, 8.0 }
+                vehicleUid = "itri-1",
+                position = new double[] { 24.773252, 121.046107, 30 }
             });
-            Console.WriteLine(JsonSerializer.Serialize(status));
+            while (true)
+            {
+                status.timestamp = DateTime.Now;
+                Console.WriteLine("sending...");
+                task=  ws.SendAsync(new ArraySegment<byte>(JsonSerializer.SerializeToUtf8Bytes(status)), WebSocketMessageType.Text, true , CancellationToken.None);
+                task.Wait();
+                Console.WriteLine("sent");
+                //Console.WriteLine(JsonSerializer.Serialize(status));
+                Thread.Sleep(1000);
+            }
         }
     }
 }
