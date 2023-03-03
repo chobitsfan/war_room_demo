a309a0045175656d5b96b0a1e8e98d624f01fc3d
Yuan-Chu Tai <chobits@itri.org.tw>
2023-03-03 16:25:18 +0800

    reply query

--- WarRoomDemo/Program.cs
+++ WarRoomDemo/Program.cs
@@ -12,54 +12,115 @@ namespace WarRoomDemo
         public string type { get; set; } = "copter";
         public string? swarmUid { get; set; }
         public bool isLeader { get; set; } = false;
+        public class Payload
+        {
+            public string type { get; set; } = "gravityBomb";
+            public int amount { get; set; }
+        }
+        public List<Payload> payloads { get; set; } = new List<Payload>();
     }
 
-    public class Status
+    public class DgsStatus
     {
         public string uid { get; set; } = "";
-        public DateTime timestamp { get; set; }
+        public DateTime timestamp { get; set; } = DateTime.Now;
         public string type { get; set; } = "dgsStatus";
-        public class StatusData
+        public class DgsStatusData
         {
             public string systemUid { get; set; } = "";
             public int ttl { get; set; } = 1;
-            public List<Vehicle> vehicles { get; set; } = new List<Vehicle>();
+            public List<Vehicle>? vehicles { get; set; }
         };
-        public StatusData data { get; set; } = new StatusData();
+        public DgsStatusData data { get; set; } = new DgsStatusData();
+    }
+
+    public class QueryReply
+    {
+        public string uid { get; set; } = "";
+        public DateTime timestamp { get; set; } = DateTime.Now;
+        public string type { get; set; } = "queryReply";
+        public class QueryReplyData
+        {
+            public string replyContent { get; set; } = "system";
+            public string replyTo { get; set; } = "";
+            public string dgsId { get; set; } = "910228";
+            public string name { get; set; } = "demo dgs";
+            public List<Vehicle>? vehicles { get; set; }
+        }
+        public QueryReplyData data { get; set;} = new QueryReplyData();
+    }
+
+    public class WarRoomPkt
+    {
+        public string uid { get; set; } = string.Empty;
+        public DateTime timestamp { get; set; }
+        public string type { get; set; } = string.Empty;
+        public JsonElement data { get; set; }
     }
 
     internal class Program
     {
-        static async Task Send(ClientWebSocket webSocket, Status status)
+        static async Task Send(ClientWebSocket webSocket, List<Vehicle> vehicles)
         {
             var jsonOpt = new JsonSerializerOptions() { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull };
             while (webSocket.State == WebSocketState.Open)
             {
-                status.timestamp = DateTime.Now;
+                DgsStatus dgsStatus = new DgsStatus();
+                dgsStatus.data.vehicles = vehicles;
                 Console.WriteLine("sendin status...");
-                await webSocket.SendAsync(new ArraySegment<byte>(JsonSerializer.SerializeToUtf8Bytes(status, jsonOpt)), WebSocketMessageType.Text, true, CancellationToken.None);
+                await webSocket.SendAsync(new ReadOnlyMemory<byte>(JsonSerializer.SerializeToUtf8Bytes(dgsStatus, jsonOpt)), WebSocketMessageType.Text, true, CancellationToken.None);
                 Console.WriteLine("status sent");
                 await Task.Delay(1000);
             }
         }
 
-        static async Task Receive(ClientWebSocket webSocket)
+        static async Task Receive(ClientWebSocket webSocket, List<Vehicle> vehicles)
         {
-            byte[] buffer = new byte[1024];
+            var jsonOpt = new JsonSerializerOptions() { DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull };
+            Memory<byte> buf = new Memory<byte>(new byte[4096]);
             while (webSocket.State == WebSocketState.Open)
             {
-                await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                var result =  await webSocket.ReceiveAsync(buf, CancellationToken.None);
+                string str = System.Text.Encoding.UTF8.GetString(buf.ToArray(), 0, result.Count);
+                Console.WriteLine("rcv msg:" + str);
+                WarRoomPkt? warRoomPkt = null;
+                try
+                {
+                    warRoomPkt = JsonSerializer.Deserialize<WarRoomPkt>(str);
+                }
+                catch (JsonException ex)
+                {
+                    Console.WriteLine($"{ex.Message}");
+                }
+                if (warRoomPkt != null)
+                {
+                    Console.WriteLine("rcv " + warRoomPkt.type);
+                    if (warRoomPkt.type == "query")
+                    {
+                        var queryContent = warRoomPkt.data.GetProperty("queryContent");
+                        if (queryContent.GetString() == "system")
+                        {
+                            Console.WriteLine("rcv system query");
+                            QueryReply queryReply = new QueryReply();
+                            queryReply.data.replyTo = warRoomPkt.uid;
+                            queryReply.data.vehicles = vehicles;
+                            await webSocket.SendAsync(new ReadOnlyMemory<byte>(JsonSerializer.SerializeToUtf8Bytes(queryReply, jsonOpt)), WebSocketMessageType.Text, true, CancellationToken.None);
+                        }
+                    }
+                }
             }
         }
 
         static void Main(string[] args)
         {
-            var status = new Status();
-            status.data.vehicles.Add(new Vehicle
+            List<Vehicle> vehicles = new()
             {
-                vehicleUid = "itri-1",
-                position = new double[] { 24.773252, 121.046107, 30 }
-            });
+                new Vehicle
+                {
+                    vehicleUid = "itri-1",
+                    position = new double[] { 24.773252, 121.046107, 30 }
+                }
+            };
 
             ClientWebSocket webSocket = new ClientWebSocket();
             Console.WriteLine("connecting...");
@@ -71,7 +132,7 @@ namespace WarRoomDemo
             webSocket.ConnectAsync(new Uri(uri), CancellationToken.None).Wait();
             Console.WriteLine("connected");
 
-            Task.WaitAll(Receive(webSocket), Send(webSocket, status));
+            Task.WaitAll(Receive(webSocket, vehicles), Send(webSocket, vehicles));
         }
     }
 }
